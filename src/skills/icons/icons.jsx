665bac684be05d4dbeb68bf749fab24d374a82a7
Willi1309 <lartiguezwilians@gmail.com>
2024-03-13 21:06:26 -0400

    se alinearon los iconos de la seccion servicios

--- src/skills/icons/icons.jsx
+++ src/skills/icons/icons.jsx
@@ -3,7 +3,7 @@ import './icons.css'
 export default function Icons ({logo, iconName, color}){
 	return(<>
 		<div className='icons'>
-			<a className='icon'>{logo}</a>
+			<a className='icon-skills'>{logo}</a>
 			<p className='iconName'>{iconName}</p>
 		</div>
 	</>)
